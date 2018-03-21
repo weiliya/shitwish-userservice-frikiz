f56460f5eda87d2ae78d0e1396f0ad8f723d2160
gelencser hanga <hangagelencser@gmail.com>
2018-03-21 18:04:23 +0100

    communicate with server

--- src/main/java/com/codecool/shitwish/api/UserServiceREST.java
+++ src/main/java/com/codecool/shitwish/api/UserServiceREST.java
@@ -8,11 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.http.*;
 import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import com.codecool.shitwish.model.Password;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.security.PermitAll;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.net.URI;
 
@@ -24,55 +29,29 @@ public class UserServiceREST {
     UserService userService;
 
 
-    @PostMapping("/user/logUser/{userId}")
+    @GetMapping("/user/log-user/{userId}")
     public ResponseEntity logUser(Model model,@PathVariable Long userId) {
         User user = userService.find(userId);
         JSONObject object = new JSONObject();
         if (userService.loginUser(userId, user.getPassword()) != null) {
             object.put("email", user.getEmail());
             object.put("userId", userId);
-            return new ResponseEntity(HttpStatus.OK);
+            return new ResponseEntity(object.toString(), HttpStatus.OK);
         } else {
             object.put("message", "Cant find a user with this email address");
-            return new ResponseEntity(HttpStatus.CONFLICT);
+            return new ResponseEntity(object.toString(), HttpStatus.CONFLICT);
         }
     }
 
-    @PostMapping("/user/regUser")
-    public ResponseEntity saveUser(Model model) {
-        final String endpoint = "";
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        JSONObject response = null;
-        headers.add("OS 10_3_1", "Safari/602.1");
-        RequestEntity<String> request = new RequestEntity<>(
-                headers, HttpMethod.GET, URI.create(endpoint));
-        JSONObject responseObject = new JSONObject();
-        String email = null;
-        String psw = null;
-        try {
-            response = restTemplate.exchange(request, JSONObject.class).getBody();
-            JSONObject object = new JSONObject(response);
-            email = object.getString("email");
-            psw = Password.hashPassword(object.getString("password"));
-        } catch (HttpClientErrorException e) {
-            System.out.println("Client side error occurred in AiController");
+    @PostMapping("/user/reg-user")
+    public ResponseEntity<HttpStatus> saveUser(@RequestBody String email, @RequestBody String password) {
+        System.out.println(email);
+        System.out.println(password);
+        return new ResponseEntity<>(HttpStatus.OK);
 
-        } catch (JSONException e) {
-            e.printStackTrace();
-            System.out.println("Json parse exception in AiController");
-        }
-        if  (userService.validateUser(email, psw)) {
-            userService.saveUser(email, psw);
-            return new ResponseEntity(HttpStatus.OK);
-
-        } else {
-            responseObject.put("message", "Already exists a user with this email address");
-            return new ResponseEntity(HttpStatus.CONFLICT);
-        }
     }
 
-    @GetMapping("/getUser/{userId}")
+    @GetMapping("/get-user/{userId}")
     public ResponseEntity getUser(Model model, @PathVariable Long userId) {
         JSONObject responseObject = new JSONObject();
         User user = userService.getUser(userId);
